b571a1de5840a0d57f921d9c21e386a7e29e7b0a
Bob Okoth <bob.okoth@student.moringaschool.com>
2021-12-12 10:36:08 +0300

    multiplication

--- index.js
+++ index.js
@@ -11,3 +11,10 @@ var subtract = function(number1,number2) {
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 alert(subtract(number1,number2));
+
+var multiply = function(number1, number2) {
+      return number1 * number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(multiply(number1, number2));
